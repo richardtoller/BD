cadb4197dba6c32965e81ed481cee66fbdacdcae
richardtoller <richardtoller@gmail.com>
2013-01-07 18:08:53 +0000

    attempting to delete output dir

--- firmware/CrossWorks/THUMB Debug/STM32_Startup.d
+++
@@ -1,3 +0,0 @@
-THUMB\ Debug/STM32_Startup.o: \
- /home/richard/.rowley_associates_limited/CrossWorks\ for\ ARM/packages/targets/STM32/STM32_Startup.s \
- /home/richard/.rowley_associates_limited/CrossWorks\ for\ ARM/packages/targets/STM32/STM32L1XX.vec
--- firmware/CrossWorks/THUMB Debug/thumb_crt0.d
+++
@@ -1,2 +0,0 @@
-THUMB\ Debug/thumb_crt0.o: \
- /usr/share/crossworks_for_arm_2.3/source/thumb_crt0.s
