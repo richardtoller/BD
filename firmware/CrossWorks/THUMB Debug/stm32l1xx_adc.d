1cc21bc4d5120e6717655ff799a5616309d1fbe7
richardtoller <richardtoller@gmail.com>
2013-01-07 18:03:05 +0000

    changes to folder structure

---
+++ firmware/CrossWorks/THUMB Debug/stm32l1xx_adc.d
@@ -0,0 +1,33 @@
+THUMB\ Debug/stm32l1xx_adc.o: \
+ /home/richard/BD/CrossWorks/../Libraries/STM32L1xx_StdPeriph_Driver/src/stm32l1xx_adc.c \
+ /home/richard/BD/CrossWorks/../Libraries/STM32L1xx_StdPeriph_Driver/inc/stm32l1xx_adc.h \
+ /home/richard/.rowley_associates_limited/CrossWorks\ for\ ARM/packages/targets/STM32/include/stm32l1xx.h \
+ /home/richard/.rowley_associates_limited/CrossWorks\ for\ ARM/packages/targets/CMSIS_3/CMSIS/Include/core_cm3.h \
+ /usr/share/crossworks_for_arm_2.3/include/stdint.h \
+ /home/richard/.rowley_associates_limited/CrossWorks\ for\ ARM/packages/targets/CMSIS_3/CMSIS/Include/core_cmInstr.h \
+ /home/richard/.rowley_associates_limited/CrossWorks\ for\ ARM/packages/targets/CMSIS_3/CMSIS/Include/core_cmFunc.h \
+ /home/richard/.rowley_associates_limited/CrossWorks\ for\ ARM/packages/targets/STM32/include/system_stm32l1xx.h \
+ /home/richard/BD/CrossWorks/../inc/stm32l1xx_conf.h \
+ /home/richard/BD/CrossWorks/../Libraries/STM32L1xx_StdPeriph_Driver/inc/stm32l1xx_crc.h \
+ /home/richard/BD/CrossWorks/../Libraries/STM32L1xx_StdPeriph_Driver/inc/stm32l1xx_opamp.h \
+ /home/richard/BD/CrossWorks/../Libraries/STM32L1xx_StdPeriph_Driver/inc/stm32l1xx_comp.h \
+ /home/richard/BD/CrossWorks/../Libraries/STM32L1xx_StdPeriph_Driver/inc/stm32l1xx_dac.h \
+ /home/richard/BD/CrossWorks/../Libraries/STM32L1xx_StdPeriph_Driver/inc/stm32l1xx_dbgmcu.h \
+ /home/richard/BD/CrossWorks/../Libraries/STM32L1xx_StdPeriph_Driver/inc/stm32l1xx_dma.h \
+ /home/richard/BD/CrossWorks/../Libraries/STM32L1xx_StdPeriph_Driver/inc/stm32l1xx_exti.h \
+ /home/richard/BD/CrossWorks/../Libraries/STM32L1xx_StdPeriph_Driver/inc/stm32l1xx_flash.h \
+ /home/richard/BD/CrossWorks/../Libraries/STM32L1xx_StdPeriph_Driver/inc/stm32l1xx_fsmc.h \
+ /home/richard/BD/CrossWorks/../Libraries/STM32L1xx_StdPeriph_Driver/inc/stm32l1xx_gpio.h \
+ /home/richard/BD/CrossWorks/../Libraries/STM32L1xx_StdPeriph_Driver/inc/stm32l1xx_syscfg.h \
+ /home/richard/BD/CrossWorks/../Libraries/STM32L1xx_StdPeriph_Driver/inc/stm32l1xx_i2c.h \
+ /home/richard/BD/CrossWorks/../Libraries/STM32L1xx_StdPeriph_Driver/inc/stm32l1xx_iwdg.h \
+ /home/richard/BD/CrossWorks/../Libraries/STM32L1xx_StdPeriph_Driver/inc/stm32l1xx_lcd.h \
+ /home/richard/BD/CrossWorks/../Libraries/STM32L1xx_StdPeriph_Driver/inc/stm32l1xx_pwr.h \
+ /home/richard/BD/CrossWorks/../Libraries/STM32L1xx_StdPeriph_Driver/inc/stm32l1xx_rcc.h \
+ /home/richard/BD/CrossWorks/../Libraries/STM32L1xx_StdPeriph_Driver/inc/stm32l1xx_rtc.h \
+ /home/richard/BD/CrossWorks/../Libraries/STM32L1xx_StdPeriph_Driver/inc/stm32l1xx_sdio.h \
+ /home/richard/BD/CrossWorks/../Libraries/STM32L1xx_StdPeriph_Driver/inc/stm32l1xx_spi.h \
+ /home/richard/BD/CrossWorks/../Libraries/STM32L1xx_StdPeriph_Driver/inc/stm32l1xx_tim.h \
+ /home/richard/BD/CrossWorks/../Libraries/STM32L1xx_StdPeriph_Driver/inc/stm32l1xx_usart.h \
+ /home/richard/BD/CrossWorks/../Libraries/STM32L1xx_StdPeriph_Driver/inc/stm32l1xx_wwdg.h \
+ /home/richard/BD/CrossWorks/../Libraries/STM32L1xx_StdPeriph_Driver/inc/misc.h
---
+++ firmware/CrossWorks/THUMB Debug/thumb_crt0.d
@@ -0,0 +1,2 @@
+THUMB\ Debug/thumb_crt0.o: \
+ /usr/share/crossworks_for_arm_2.3/source/thumb_crt0.s
